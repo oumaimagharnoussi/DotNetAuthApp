90a722dbc2bc2f69ae0c48ff64a5abb734ac05ce
oumaimagharnoussi <gharnoussioumaima@gmail.com>
2023-01-10 14:11:10 +0100

    Encrypt Password

--- BackAppPFE/Controllers/UserController.cs
+++ BackAppPFE/Controllers/UserController.cs
@@ -1,8 +1,11 @@
 ﻿using BackAppPFE.Context;
+using BackAppPFE.Helpers;
 using BackAppPFE.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace BackAppPFE.Controllers
 {
@@ -35,7 +38,21 @@ namespace BackAppPFE.Controllers
         {
             if (userObj == null)
                 return BadRequest();
+            //check username 
+            if (await CheckUserNameExistAsync(userObj.Username))
+                return BadRequest(new { Message = "Username Already Exist!" });
+            //check email
+            if (await CheckEmailExistAsync(userObj.Email))
+                return BadRequest(new { Message = "Email Already Exist!" });
+            //check password
+            var pass = CheckPasswordStrength(userObj.Password);
+            if (!string.IsNullOrEmpty(pass))
+                return BadRequest(new { Message = pass.ToString() });
 
+
+            userObj.Password = PasswordHasher.HashPassword(userObj.Password);
+            userObj.Role = "User";
+            userObj.Token = "";
             await _authContext.Users.AddAsync(userObj);
             await _authContext.SaveChangesAsync();
             return Ok(new
@@ -43,5 +60,25 @@ namespace BackAppPFE.Controllers
                 Message = "User Registered!"
             });
         }
+
+        private Task<bool> CheckUserNameExistAsync(string username)
+          => _authContext.Users.AnyAsync(x => x.Username == username);
+
+        private Task<bool> CheckEmailExistAsync(string email)
+          => _authContext.Users.AnyAsync(x => x.Email == email);
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (password.Length < 8)
+                sb.Append("Minimum password lenght should be 8" + Environment.NewLine);
+            if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]")
+                && Regex.IsMatch(password, "[0-9]")))
+                sb.Append("Password should be Alphanumeric" + Environment.NewLine);
+            if (!Regex.IsMatch(password, "[<,>,@,!,#,$,%,^,&,*,(,),_,+,\\[,\\],{,},?,:,;,|,',\\,.,/,`,-,=]"))
+                sb.Append("Password should contain special chars" + Environment.NewLine);
+            return sb.ToString();
         }
+
+    }
+
 }
