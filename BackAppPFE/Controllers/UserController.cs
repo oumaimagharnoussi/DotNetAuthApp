037691c453ff9f67086b84cb4d683db5150a4b0c
oumaimagharnoussi <gharnoussioumaima@gmail.com>
2023-01-10 12:07:44 +0100

    Authentication and Authorization

---
+++ BackAppPFE/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using BackAppPFE.Context;
+using BackAppPFE.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackAppPFE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public UserController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;
+        }
+        [HttpPost("authentificate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+            var user = await _authContext.Users
+                .FirstOrDefaultAsync(x => x.Username == userObj.Username);
+            if (user == null)
+                return NotFound(new { Message = "User Not Found!" });
+            return Ok(new
+            {
+          
+                Message = "Login Success!"
+            });
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User Registered!"
+            });
+        }
+        }
+}
